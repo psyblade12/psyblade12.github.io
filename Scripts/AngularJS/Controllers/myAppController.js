9dd87aac4b21cd7438b485a3fcadf58d28c18779
Psyblade <ak.shootgun@gmail.com>
2018-07-29 02:30:54 +0700

    Some work

--- Scripts/AngularJS/Controllers/myAppController.js
+++ Scripts/AngularJS/Controllers/myAppController.js
@@ -1,58 +1,57 @@
-var app = angular.module('myApp', []);
-    app.controller('myCtrl', function($scope, $http) {
-        $scope.LoadTable = function (){
-            $scope.isLoading = true;
-            $http.get(apiRoute+"File/ReadTextFile?fileName="+fileName).then(function(response) {
-                var temp = response.data;
-                $scope.names = JSON.parse(temp);
-                $scope.namesToRevert = JSON.parse(temp);
-                $scope.isLoading = false;
-            });
+app.controller('myCtrl', function($scope, $http) {
+    $scope.LoadTable = function (){
+        $scope.isLoading = true;
+        $http.get(apiRoute+"File/ReadTextFile?fileName="+fileName).then(function(response) {
+            var temp = response.data;
+            $scope.names = JSON.parse(temp);
+            $scope.namesToRevert = JSON.parse(temp);
+            $scope.isLoading = false;
+        });
+    }
+    
+    $scope.AddNewPerson = function (){
+        var aPerson = {
+            name: null,
+            age: null,
+        };
+        $scope.names.push(aPerson);
+    }
+
+    $scope.RevertChange = function (){
+        $scope.names = $scope.namesToRevert.map(x=> Object.assign({}, x) );
+    }
+
+    $scope.DeleteAPerson = function (index){
+        $scope.names.splice(index,1);
+    }
+
+    $scope.SaveToServer = function(){
+        if(!this.tableofPeople.$dirty && ($scope.names.length == $scope.namesToRevert.length)){
+            alert("You have not changed anything");
+            return;
         }
-        
-        $scope.AddNewPerson = function (){
+        $scope.isLoading = true;
+        var dataToPost = $scope.names.map(function(x){
             var aPerson = {
-                name: null,
-                age: null,
-            };
-            $scope.names.push(aPerson);
-        }
-
-        $scope.RevertChange = function (){
-            $scope.names = $scope.namesToRevert.map(x=> Object.assign({}, x) );
-        }
-
-        $scope.DeleteAPerson = function (index){
-            $scope.names.splice(index,1);
-        }
-
-        $scope.SaveToServer = function(){
-            if(!this.tableofPeople.$dirty && ($scope.names.length == $scope.namesToRevert.length)){
-                alert("You have not changed anything");
-                return;
+                name: x.name,
+                age:  parseInt(x.age),
             }
-            $scope.isLoading = true;
-            var dataToPost = $scope.names.map(function(x){
-                var aPerson = {
-                    name: x.name,
-                    age:  parseInt(x.age),
-                }
-                return aPerson;
-            })
+            return aPerson;
+        })
 
-            $http.post(apiRoute+"File/OverwriteTextFile?fileName="+fileName, dataToPost).then(function(response) {
-                $scope.LoadTable();
-                $scope.isLoading = false;
-            });
-            
-            this.tableofPeople.$dirty = false;
-        }
+        $http.post(apiRoute+"File/OverwriteTextFile?fileName="+fileName, dataToPost).then(function(response) {
+            $scope.LoadTable();
+            $scope.isLoading = false;
+        });
+        
+        this.tableofPeople.$dirty = false;
+    }
 
-        $scope.ToggleAddButton = function (item){
-            $scope.AddButtonEnable = !$scope.AddButtonEnable;
-        }
+    $scope.ToggleAddButton = function (item){
+        $scope.AddButtonEnable = !$scope.AddButtonEnable;
+    }
 
-        $scope.AddButtonEnable = false;
+    $scope.AddButtonEnable = false;
 
-        $scope.LoadTable();
-    });
+    $scope.LoadTable();
+});
--- Scripts/AngularJS/MainApp.js
+++ Scripts/AngularJS/MainApp.js
@@ -1,1 +1,15 @@
-var app = angular.module('myApp', []);
+var app = angular.module('myApp', ["ngRoute"]);
+app.config(function($routeProvider) {
+    $routeProvider
+    .when("/", {
+        templateUrl : "Main.html",
+        controller: "myCtrl"
+    })
+    .when("/about", {
+        templateUrl : "About.html"
+    })
+    .otherwise({
+        template : "<h5>Nothing for this route</h5>"
+    });
+});
+        
